9252475eb7ce0e8ed2bb7305f357bf6f887f7616
PavelGunko <g9636406069@yandex.ru>
2022-10-16 19:31:57 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int[] sales = {10, 20, 30};
+        SalesManager SalesManager = new SalesManager(sales);
+        System.out.println(SalesManager.max());
+
+    }
+}
